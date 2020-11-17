f3c56a9611871159c05d1e4557fe653c3519562f
Kradeyes <yanin.r.a@mail.ru>
2020-11-17 14:48:52 +0300

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3[285210#322234]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -5,16 +5,37 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
+
 import static ru.job4j.html.DateUtils.changeData;
 
 public class SqlRuParse {
 
     public static void main(String[] args) throws Exception {
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        Elements row = doc.select(".postslisttopic");
-        Elements date = doc.select(".altCol");
-        Iterator<Element> iterator = date.iterator();
+        List<String> urls = pageCounter();
+        for (String url: urls) {
+            Document doc = Jsoup.connect(url).get();
+            Elements row = doc.select(".postslisttopic");
+            Elements date = doc.select(".altCol");
+            Iterator<Element> iterator = date.iterator();
+            printAllPages(row, iterator);
+        }
+    }
+
+    public static List<String> pageCounter() {
+        String url = "https://www.sql.ru/forum/job-offers/";
+        List<String> urls = new ArrayList<>();
+        int firstPage = 1;
+        int lastPage = 6;
+        for (int i = firstPage; i < lastPage; i++) {
+            urls.add(url + i);
+        }
+        return urls;
+    }
+
+    public static void printAllPages(Elements row, Iterator<Element> iterator) {
         for (Element td : row) {
             Element href = td.child(0);
             System.out.println(href.attr("href"));
