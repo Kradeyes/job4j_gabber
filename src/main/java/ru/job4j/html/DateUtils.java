40a30db11fd84655a7fd1a68990a7171ccddc1f6
Kradeyes <yanin.r.a@mail.ru>
2020-11-18 12:16:59 +0300

    2.1. Преобразование даты[289476#322238] #3

--- src/main/java/ru/job4j/html/DateUtils.java
+++ src/main/java/ru/job4j/html/DateUtils.java
@@ -8,27 +8,30 @@ import java.util.Map;
 import static java.util.Map.entry;
 
 public class DateUtils {
+
+    private static final Map<String, String> MONTHS = Map.ofEntries(
+            entry("янв", "янв."),
+            entry("фев", "февр."),
+            entry("мар", "мар."),
+            entry("апр", "апр."),
+            entry("май", "мая."),
+            entry("июн", "июн."),
+            entry("июл", "июл."),
+            entry("авг", "авг."),
+            entry("сен", "сент."),
+            entry("окт", "окт."),
+            entry("ноя", "нояб."),
+            entry("дек", "дек.")
+    );
+
     public static LocalDate changeData(String s) {
-        final Map<String, String> months = Map.ofEntries(
-                entry("янв", "янв."),
-                entry("фев", "февр."),
-                entry("мар", "мар."),
-                entry("апр", "апр."),
-                entry("май", "мая."),
-                entry("июн", "июн."),
-                entry("июл", "июл."),
-                entry("авг", "авг."),
-                entry("сен", "сент."),
-                entry("окт", "окт."),
-                entry("ноя", "нояб."),
-                entry("дек", "дек.")
-        );
+
         String[] split = s.split(" ");
         if (split.length == 2) {
             LocalDate localDate = LocalDate.now();
             return s.contains("сегодня") ? localDate : localDate.minusDays(1);
         } else {
-            String txt = s.replace(split[1], months.get(split[1]));
+            String txt = s.replace(split[1], MONTHS.get(split[1]));
             DateTimeFormatter formatter = DateTimeFormatter
                     .ofPattern("d MMM yy, HH:mm", new Locale("ru"));
             return LocalDate.parse(txt, formatter);
