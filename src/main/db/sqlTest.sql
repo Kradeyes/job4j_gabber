4bb1be1b743c1203d77dab24fd4eb8fb12450f1a
Kradeyes <yanin.r.a@mail.ru>
2020-12-08 17:49:03 +0300

    Экзамен. Sql[1730#322253] remaster

--- src/main/db/sqlTest.sql
+++ src/main/db/sqlTest.sql
@@ -1,32 +1,44 @@
 create table users (
-    id int primary key,
+    id serial primary key,
     name varchar(255)
 );
 
-create table meeting
-(
-    id      int primary key,
-    name    varchar(255)
+create table meeting (
+    id serial primary key,
+    name varchar(255)
 );
 
 create table validate (
-    id  int primary key,
+    id serial primary key,
     user_id int references users(id),
-    meeting_id int references meeting(id)
+    meeting_id int references meeting(id),
+    status varchar(255) default('Unread')
 );
 
-insert into users values (1, 'Roma');
-insert into users values (2, 'Sasha');
-insert into users values (3, 'Vasya');
+insert into users(name) values('Roma');
+insert into users(name) values('Petr');
+insert into users(name) values('Sasha');
 
-insert into meeting values (1,'Planner');
-insert into meeting values (2,'Corporate');
-insert into meeting values (3,'Journey');
 
-insert into validate values (1,  1, 1);
-insert into validate values (2,  1, 2);
-insert into validate values (3,  2, 1);
-insert into validate values (4,  3, 2);
+insert into meeting(name) values ('Planner');
+insert into meeting(name) values ('Corporate');
+insert into meeting(name) values ('Journey');
+
+insert into validate(user_id, meeting_id) values (1, 1);
+insert into validate(user_id, meeting_id) values (1, 2);
+
+insert into validate(user_id, meeting_id) values (2, 1);
+insert into validate(user_id, meeting_id) values (2, 1);
+
+insert into validate(user_id, meeting_id) values (3, 3);
+insert into validate(user_id, meeting_id) values (3, 1);
+
+update validate set  status = 'Yes' where id=1;
+update validate set  status = 'No' where id=2;
+update validate set  status = 'Yes' where id=3;
+update validate set  status = 'No' where id=4;
+update validate set  status = 'Yes' where id=5;
+
 
 select count(id) as Applications, count(distinct user_id) as Users from validate;
 
